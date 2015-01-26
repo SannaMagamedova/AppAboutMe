0b6a2782b59801ef46a5699139d640a783c61691
ITHS2012 <iths2012@ITHS2012s-MacBook-Pro-2.local>
2015-01-26 15:36:15 +0100

    First commit

---
+++ Me/Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Me
+//
+//  Created by ITHS2012 on 2015-01-25.
+//  Copyright (c) 2015 ITHS2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Me/Me/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Me
+//
+//  Created by ITHS2012 on 2015-01-25.
+//  Copyright (c) 2015 ITHS2012. All rights reserved.
+//
+
+#import "ViewController.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
